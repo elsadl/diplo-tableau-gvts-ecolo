73585df90c1aca58c3be88d59227912f62501be4
Elsa <58072730+elsadl@users.noreply.github.com>
2021-11-28 18:05:01 +0100

    fix mobile?

--- script.js
+++ script.js
@@ -26,9 +26,8 @@ const createTableau = () => {
   addNotes();
   tableau.style.display = "grid";
   fixPositionTooltips();
-  tableauWidth = tableau.getBoundingClientRect().width + 20;
+  tableauWidth = tableau.getBoundingClientRect().width + 40;
   scaleTableau();
-  if (mobile) fixHoverOnMobile();
 };
 
 const fillTableau = () => {
@@ -168,17 +167,6 @@ const fixPositionTooltips = () => {
       translateY = "calc(-100% + 1.6em)";
     }
     el.style.transform = `translate(${translateX}, ${translateY})`;
-  });
-};
-
-const fixHoverOnMobile = () => {
-  Array.from(document.querySelectorAll(".notes")).forEach((el) => {
-    el.addEventListener("click", () => {
-      Array.from(document.querySelectorAll(".notes")).forEach((note) => {
-        note.classList.remove("hovered");
-      });
-      el.classList.add("hovered");
-    });
   });
 };
 
